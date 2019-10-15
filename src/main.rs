75e6696189a6baeeb93a5beb93e2c129368edf04
Rob Saunders <rob@wearebrandnew.com>
2019-10-15 18:38:16 +0800

    removed cases where output would double up

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,3 @@
-use log::*;
-
 fn main() {
-    let result = cdd::run();
-    // match result {
-    //     Ok(_) => (),
-    //     Err(err) => {
-    //         for line in format!("{}", err).lines() {
-    //             error!("{}", line)
-    //         }
-    //     }
-    // };
+    let _ = cdd::run();
 }
