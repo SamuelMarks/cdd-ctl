3ba8c02f912ad155a06c13386eb0cc48e33c1ded
Rob Saunders <rob@wearebrandnew.com>
2019-10-20 18:05:29 +0800

    prints success message if the entire process was successful

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,9 @@ use log::*;
 fn main() {
     if let Err(err) = cdd::run() {
         for line in format!("{}", err).lines() {
-            error!("{}", line)
+            error!("{}", line);
         }
+    } else {
+        info!("Sync successful");
     }
 }
