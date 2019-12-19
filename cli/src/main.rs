5d18cb0224109ddb78d431b9d0ca1b23aef57bf3
Rob Saunders <rob@wearebrandnew.com>
2019-12-19 08:30:45 +0800

    cli project calls cdd lib

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,3 +1,12 @@
+use cdd;
+use log::*;
+
 fn main() {
-    println!("Hello, world!");
+    if let Err(err) = cdd::run() {
+        for line in format!("{}", err).lines() {
+            error!("[CDD] {}", line);
+        }
+    } else {
+        info!("Sync successful");
+    }
 }
